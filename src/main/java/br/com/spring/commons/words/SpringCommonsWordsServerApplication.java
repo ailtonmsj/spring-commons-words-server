9419095b2f651c4d47aae1fcd2f618ee7e558ece
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2018-12-22 21:19:51 -0200

    Initial commit

---
+++ src/main/java/br/com/spring/commons/words/SpringCommonsWordsServerApplication.java
@@ -0,0 +1,16 @@
+package br.com.spring.commons.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCommonsWordsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommonsWordsServerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/spring/commons/words/SpringCommonsWordsServerApplicationTests.java
@@ -0,0 +1,19 @@
+package br.com.spring.commons.words;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SpringCommonsWordsServerApplication)
+@ActiveProfiles("local-test")
+public class SpringCommonsWordsServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
